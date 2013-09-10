afdd576cb01e142fc63a231bea3fe420ab3d2848
NickPfeufer <nicholaspfeufer@msn.com>
2013-09-09 19:47:59 -0600

    iteration count

--- exersize5.java
+++ exersize5.java
@@ -7,6 +7,7 @@ class exersize5 {
 	}
 
 	public static void hailstone(int x){
+	    int iterations = 0;
 	    while (x > 1){
 	    
 		   if (x%2==0 && x>1) {
@@ -14,19 +15,17 @@ class exersize5 {
 	        	System.out.print(x);
         	        System.out.println("\teven, next value is"+ x+"/2");
 		        x = x/2;
-        	    };
-
-
-	            if (x%2!=0 && x>1) {
+        	    }else if (x%2!=0 && x>1) {
 
 		        System.out.print(x);
 	                System.out.println("\todd, next value is" + "3*" + x + " +1");
 	                x=(3*x)+1;
 	
 		    };
+		    iterations++;
 	    }
             System.out.print(1);
-            System.out.println("\tstop calculation");
+            System.out.println("\tstop calculation. " + iterations + "iterations");
 
 	}
 

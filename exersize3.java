eedb4624352b45ec629d8731c0331cb87b13d131
NickPfeufer <nicholaspfeufer@msn.com>
2013-09-09 18:57:20 -0600

    Update exersize3.java

--- exersize3.java
+++ exersize3.java
@@ -1,30 +1,34 @@
 class exersize3 {
-	
-	public static void main(String[] args) {
-		etothex(2);
-	}
 
-	public static void etothex(double x){
-		double n = 1.0;
-		double answer = n * x;
-		double term = x;
-		System.out.println(1);
-		System.out.println(term);
-		System.out.println(answer);
-		System.out.println(2);
-		System.out.println(term);		
-		System.out.println(answer);
-		
-		n = 3; 
-		for (int i = 2; i < 20; i++) {
-			answer += term;
-			term = ((x * Math.exp(n-1))/(n-1)) * (x/n);
-			n++;
-			System.out.println(i+1);
-			System.out.println(term);		
-			System.out.println(answer);
-		};
+    public static void main(String[] args) {
+        etothex(2);
+    }
 
-	}
+
+    public static void etothex(double x){
+        double n = 1.0;
+        double answer = n * x;
+        double term = x;
+
+        System.out.println(1);
+        System.out.println(term);
+        System.out.println(answer);
+
+        n = 2;
+
+        for (int i = 1; i < 20; i++) {
+
+            answer += term;
+            term = ((x * Math.exp(n-1))/(n-1)) * (x/n);
+            n++;
+
+            System.out.println(i+1);
+            System.out.println(term);        
+            System.out.println(answer);
+
+        };
+
+
+    }
 
 }

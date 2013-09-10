198ece48e55f2e316d714416ec89ce3911b1d62d
NickPfeufer <nicholaspfeufer@msn.com>
2013-09-09 18:53:22 -0600

    Update exersize3.java

--- exersize3.java
+++ exersize3.java
@@ -1,24 +1,28 @@
-import   java.lang.Math.*;
-
 class exersize3 {
 	
 	public static void main(String[] args) {
 		etothex(2);
 	}
 
-	public static double etothex(double x){
+	public static void etothex(double x){
 		double n = 1.0;
 		double answer = n * x;
 		double term = x;
-		double twelth = Math.exp(-12.0);
+		System.out.println(1);
+		System.out.println(term);
 		System.out.println(answer);
+		System.out.println(2);
+		System.out.println(term);		
 		System.out.println(answer);
+		
 		n = 3; 
-		while (term > twelth) {
-			answer += answer + term;
+		for (int i = 2; i < 20; i++) {
+			answer += term;
 			term = ((x * Math.exp(n-1))/(n-1)) * (x/n);
 			n++;
-		System.out.println(answer);
+			System.out.println(i+1);
+			System.out.println(term);		
+			System.out.println(answer);
 		};
 
 	}

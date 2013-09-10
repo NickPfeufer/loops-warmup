bb99e560fd66b8af15b91d77145ca561621029e3
NickPfeufer <nicholaspfeufer@msn.com>
2013-09-09 18:43:25 -0600

    Update exersize3.java

--- exersize3.java
+++ exersize3.java
@@ -6,14 +6,17 @@ class exersize3 {
 
 	public static double etothex(double x){
 		double n = 1.0;
-		double answer = n + x;
+		double answer = n * x;
 		double term = x;
 		double twelth = Math.exp(-12.0);
+		System.out.println(answer);
+		System.out.println(answer);
 		n = 3; 
 		while (term > twelth) {
 			answer += answer + term;
 			term = ((x * Math.exp(n-1))/(n-1)) * (x/n);
 			n++;
+		System.out.println(answer);
 		};
 
 	}
